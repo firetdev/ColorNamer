e3b6f7b677a0a341275ef7fd804fa5d59f78a5a7
flesheatindragon <futuregamesstore@gmail.com>
2024-10-27 21:25:11 -0500

    Began processing of values

--- web/main.js
+++ web/main.js
@@ -1,5 +1,15 @@
 // Main script
 
+// Color object
+function Clr(red, green, blue) {
+    this.r = red;
+    this.g = green;
+    this.b = blue;
+}
+
+// Distance function
+function distance
+
 // Main function
 function main() {
     let color = document.getElementById('colorInput').value;
@@ -17,6 +27,13 @@ function main() {
             let text2 = '';  // Just the color names
             text1 = rawTrainingText.replace(new RegExp('([A-Za-z]+)', 'g'), '');
             text2 = rawTrainingText.replace(new RegExp('\\d{1,3}, \\d{1,3}, \\d{1,3}', 'g'), '');
+            let rgbValues = text1.split('\n');
+            let colors = text2.split('\n');
+            let colorArray = [];
+            for (let i = 0; i < rgbValues.length; i += 1) {
+                let numbers = rgbValues[i].match(new RegExp('(\\d{1-3},)', 'g'));
+                colorArray.push(new Clr(numbers[0], numbers[1], numbers[2]));
+            }
         })
         .catch((error) => {
             console.error(error);
